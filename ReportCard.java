f09a7e7c018e1093aa0bbc87b1f60e4228ea4fd2
Ragheed Ma'rouf Huneineh <ragheedmhuneineh@outlook.com>
2017-04-28 00:57:20 +0200

    Apply requirements and suggestions

--- ReportCard.java
+++ ReportCard.java
@@ -16,73 +16,120 @@ public class ReportCard {
      * The marks/average for the courses that must be taken by the student are set as double and initialized
      * to 0.
      */
-    private double mEnglishMark = 0;
-    private double mMathMark = 0;
-    private double mPhysicsMark = 0;
-    private double mChemistryMark = 0;
-    private double mBiologyMark = 0;
-    private double mGeographyMark = 0;
-    private double mHistoryMark = 0;
+    private double mEnglishMark;
+    private double mMathMark;
+    private double mPhysicsMark;
+    private double mChemistryMark;
+    private double mBiologyMark;
+    private double mGeographyMark;
+    private double mHistoryMark;
     private double mAvg;
 
     /**
      * Average is considered as a weighted average for courses' marks. Weight is an integer and is
      * constant for every subject.
      */
-    private int englishWeight = 5;
-    private int mathWeight = 7;
-    private int physicsWeight = 6;
-    private int chemistryWeight = 5;
-    private int biologyWeight = 6;
-    private int geographyWeight = 3;
-    private int historyWeight = 3;
+    private final int ENGLISH_WEIGHT = 5;
+    private final int MATH_WEIGHT = 7;
+    private final int PHYSICS_WEIGHT = 6;
+    private final int CHEMISTRY_WEIGHT = 5;
+    private final int BIOLOGY_WEIGHT = 6;
+    private final int GEOGRAPHY_WEIGHT = 3;
+    private final int HISTORY_WEIGHT = 3;
 
     /**
      * Constructor only requires student's name and surname
-     * @param name
-     * @param surname
+     * @param name student name
+     * @param surname student surname
+     * @param englishMark student english mark
+     * @param mathMark student math mark
+     * @param physicsMark student physics mark
+     * @param chemistryMark student chemistry mark
+     * @param biologyMark student biology mark
+     * @param geographyMark student geography mark
+     * @param historyMark student history mark
      */
-    public ReportCard(String name, String surname){
+    public ReportCard(String name, String surname, double englishMark, double mathMark, double physicsMark,
+                      double chemistryMark, double biologyMark, double geographyMark, double historyMark){
         mName = name;
         mSurname = surname;
+        mEnglishMark = englishMark;
+        mMathMark = mathMark;
+        mPhysicsMark = physicsMark;
+        mChemistryMark = chemistryMark;
+        mBiologyMark = biologyMark;
+        mGeographyMark = geographyMark;
+        mHistoryMark = historyMark;
+
+
     }
 
     /**
      * Setters set the mark of the student for each subject alone.
      */
-    public void setEnglishMark(double mark) { mEnglishMark = mark; }
-    public void setMathMark(double mark) { mMathMark = mark; }
-    public void setPhysicsMark(double mark) { mPhysicsMark = mark; }
-    public void setChemistryMark(double mark) { mChemistryMark = mark; }
-    public void setBiologyMark(double mark) { mBiologyMark = mark; }
-    public void setGeographyMark(double mark) { mGeographyMark = mark; }
-    public void setHistoryMark(double mark) { mHistoryMark = mark; }
+    public void setEnglishMark(double mark) {
+        mEnglishMark = mark; }
+
+    public void setMathMark(double mark) {
+        mMathMark = mark; }
+
+    public void setPhysicsMark(double mark) {
+        mPhysicsMark = mark; }
+
+    public void setChemistryMark(double mark) {
+        mChemistryMark = mark; }
+
+    public void setBiologyMark(double mark) {
+        mBiologyMark = mark; }
+
+    public void setGeographyMark(double mark) {
+        mGeographyMark = mark; }
+
+    public void setHistoryMark(double mark) {
+        mHistoryMark = mark; }
 
 
     /**
      * Getters
-     * @return
      */
-    public String getName(){ return mName; }
-    public String getSurname(){ return mSurname; }
-    public double getEnglishMark(){ return mEnglishMark; }
-    public double getMathMark(){ return mMathMark; }
-    public double getPhysicsMark(){ return mPhysicsMark; }
-    public double getChemistryMark(){ return mChemistryMark; }
-    public double getBiologyMark(){ return mBiologyMark; }
-    public double getGeographyMark(){ return mGeographyMark; }
-    public double getHistoryMark(){ return mHistoryMark; }
-    public double getAverage(){
-        double totalScore = mEnglishMark*englishWeight +
-                mMathMark*mathWeight +
-                mPhysicsMark*physicsWeight +
-                mChemistryMark*chemistryWeight +
-                mBiologyMark*biologyWeight +
-                mGeographyMark*geographyWeight +
-                mHistoryMark*historyWeight;
+    public String getName(){
+        return mName; }
 
-        int totalWeight = englishWeight + mathWeight + physicsWeight + chemistryWeight + biologyWeight +
-                geographyWeight + historyWeight;
+    public String getSurname(){
+        return mSurname; }
+
+    public double getEnglishMark(){
+        return mEnglishMark; }
+
+    public double getMathMark(){
+        return mMathMark; }
+
+    public double getPhysicsMark(){
+        return mPhysicsMark; }
+
+    public double getChemistryMark(){
+        return mChemistryMark; }
+
+    public double getBiologyMark(){
+        return mBiologyMark; }
+
+    public double getGeographyMark(){
+        return mGeographyMark; }
+
+    public double getHistoryMark(){
+        return mHistoryMark; }
+
+    public double getAverage(){
+        double totalScore = mEnglishMark*ENGLISH_WEIGHT +
+                mMathMark*MATH_WEIGHT +
+                mPhysicsMark*PHYSICS_WEIGHT +
+                mChemistryMark*CHEMISTRY_WEIGHT +
+                mBiologyMark*BIOLOGY_WEIGHT +
+                mGeographyMark*GEOGRAPHY_WEIGHT +
+                mHistoryMark*HISTORY_WEIGHT;
+
+        int totalWeight = ENGLISH_WEIGHT + MATH_WEIGHT + PHYSICS_WEIGHT + CHEMISTRY_WEIGHT + BIOLOGY_WEIGHT +
+                GEOGRAPHY_WEIGHT + HISTORY_WEIGHT;
 
         mAvg = totalScore/totalWeight;
 
@@ -91,7 +138,14 @@ public class ReportCard {
 
     @Override
     public String toString(){
-        String result = mName + " " + mSurname + "\'s Average = " + mAvg;
+        String result = mName + " " + mSurname + "\'s Average = " + mAvg +
+                "\nEnglish: " + mEnglishMark +
+                "\nMath: " + mMathMark +
+                "\nPhysics: " + mPhysicsMark +
+                "\nChemistry: " + mChemistryMark +
+                "\nBiology: " + mBiologyMark +
+                "\nGeography: : " + mGeographyMark +
+                "\nHistory: " + mHistoryMark;
 
         return result;
     }
